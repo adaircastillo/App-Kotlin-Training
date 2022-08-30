8d7444f4217e2017fe39e8ceb7b2ca0fb1e2db24
Adair de Jesus Castillo Meza <nemesis2074@gmail.com>
2022-08-29 21:17:50 -0500

    new endpoint added

--- app/src/main/java/net/nemesis/contacts/model/services/Endpoints.kt
+++ app/src/main/java/net/nemesis/contacts/model/services/Endpoints.kt
@@ -2,4 +2,5 @@ package net.nemesis.contacts.model.services
 
 object Endpoints {
     const val BASE = "https://mi-docencia-benv.web.app"
+    const val BASE_MESSAGES = "https://keratinous-enclosur.000webhostapp.com"
 }
